605bc636ea73c4b6279379af9b31e4dc7fcf0a93
joybappy2 <fan328547@gmail.com>
2025-08-28 15:32:17 +0600

    feature added heart button click will increase heart number value in navbar & class added to call buttons

--- main.js
+++ main.js
@@ -1,11 +1,14 @@
-//  FUNCTIONS
-// function to get element 
-function getElement( id ) {
-    return document.getElementById(id)
-}
 
-const cardHeart = getElement( "card-heart" )
-console.log( cardHeart )
-cardHeart.addEventListener( "click", function () {
-    console.log("heart clicked")
-})
+// Increasing Heart Number Value if Heart Icon Clicked
+const heartIcons = document.querySelectorAll( ".heart-icon" )
+let newHeartCountNumber = 0;
+heartIcons.forEach( function ( heartIcon ) {
+    heartIcon.addEventListener( "click", function () {
+        console.log( "heart icon clicked" )
+        const heartCountEl = document.getElementById( "heart-count" )
+        console.log( heartCountEl )
+        newHeartCountNumber += 1
+        heartCountEl.innerText = newHeartCountNumber
+    })
+} )
+
