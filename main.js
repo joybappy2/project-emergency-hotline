17552cfb9c4aed9e2af892b08c76349059fcc035
joybappy2 <fan328547@gmail.com>
2025-08-28 19:15:43 +0600

    feature: call will reduce 20 coin

--- main.js
+++ main.js
@@ -11,13 +11,33 @@ heartIcons.forEach( function ( heartIcon ) {
 } )
 
 
+// function to get innerText
+function getElement( id ) {
+    return document.getElementById(id)
+}
+// function to convert string to number
+function getNumber( text ) {
+    return parseInt(text)
+}
+
 // showing alert on call button click with service name and number
+const callPrice = 20
 const callBtns = document.querySelectorAll( ".call-button" )
 callBtns.forEach( function ( btn ) {
     btn.addEventListener( "click", function () {
         const cardItems = btn.parentNode.parentNode.childNodes
         const serviceName = cardItems[ 7 ].innerText
-        const serviceNumber = cardItems[11].innerText
-        alert(`📞 Calling ${serviceName}    ${serviceNumber}...`)
+        const serviceNumber = cardItems[ 11 ].innerText
+        const coinElement = getElement( "coin-count" )
+        const totalCoin = parseInt(coinElement.innerText)
+        // console.log( newTotalCoin )
+        const newTotalCoin = totalCoin - callPrice
+        console.log(totalCoin)
+        if ( totalCoin < 20 ) {
+            alert( "❌ Insufficient Coin. Need Atleast 20 Coin To Make A Call." )
+            return
+        }
+        coinElement.innerText = newTotalCoin
+        alert( `📞 Calling ${ serviceName }    ${ serviceNumber }...` )
     })
 })
