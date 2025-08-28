5548fbae12f80c22ddd521c34992fe9f672cf486
joybappy2 <fan328547@gmail.com>
2025-08-28 16:55:18 +0600

    call button functionality added

--- main.js
+++ main.js
@@ -12,3 +12,30 @@ heartIcons.forEach( function ( heartIcon ) {
     })
 } )
 
+// const emergencyCard = document.getElementsByClassName( "emergency-card" )
+// console.log(emergencyCard)
+
+// const card1 = document.getElementById( "card1" )
+// card1.addEventListener( "click", function ( event ) {
+//     const targetEvent = event.target
+//     const targetClassList = event.target.classList
+//     if ( targetClassList[ 0 ] === "call-button" ) {
+//         alert("calling national emergency 999")
+//         console.log("Got The call Btn")
+//     }
+    
+// } )
+
+
+const callBtns = document.querySelectorAll( ".call-button" )
+callBtns.forEach( function ( btn ) {
+    btn.addEventListener( "click", function () {
+        console.log("call btn clicked")
+        const cardItems = btn.parentNode.parentNode.childNodes
+        const serviceName = cardItems[ 7 ]
+        const serviceNumber = cardItems[11].innerText
+        console.log(serviceNumber)
+        const serviceNameText =  serviceName.innerText
+        alert(`📞 Calling ${serviceNameText} ${serviceNumber}`)
+    })
+})
