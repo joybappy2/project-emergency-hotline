e60e8e8995c802ad75e463c6c568c45438e1479f
joybappy2 <fan328547@gmail.com>
2025-08-28 17:44:09 +0600

    extra code & console removed

--- main.js
+++ main.js
@@ -1,41 +1,23 @@
 
 // Increasing Heart Number Value if Heart Icon Clicked
+let heartCount = 0;
 const heartIcons = document.querySelectorAll( ".heart-icon" )
-let newHeartCountNumber = 0;
 heartIcons.forEach( function ( heartIcon ) {
     heartIcon.addEventListener( "click", function () {
-        console.log( "heart icon clicked" )
+        heartCount += 1
         const heartCountEl = document.getElementById( "heart-count" )
-        console.log( heartCountEl )
-        newHeartCountNumber += 1
-        heartCountEl.innerText = newHeartCountNumber
+        heartCountEl.innerText = heartCount
     })
 } )
 
-// const emergencyCard = document.getElementsByClassName( "emergency-card" )
-// console.log(emergencyCard)
 
-// const card1 = document.getElementById( "card1" )
-// card1.addEventListener( "click", function ( event ) {
-//     const targetEvent = event.target
-//     const targetClassList = event.target.classList
-//     if ( targetClassList[ 0 ] === "call-button" ) {
-//         alert("calling national emergency 999")
-//         console.log("Got The call Btn")
-//     }
-    
-// } )
-
-
+// showing alert on call button click with service name and number
 const callBtns = document.querySelectorAll( ".call-button" )
 callBtns.forEach( function ( btn ) {
     btn.addEventListener( "click", function () {
-        console.log("call btn clicked")
         const cardItems = btn.parentNode.parentNode.childNodes
-        const serviceName = cardItems[ 7 ]
+        const serviceName = cardItems[ 7 ].innerText
         const serviceNumber = cardItems[11].innerText
-        console.log(serviceNumber)
-        const serviceNameText =  serviceName.innerText
-        alert(`📞 Calling ${serviceNameText} ${serviceNumber}`)
+        alert(`📞 Calling ${serviceName}    ${serviceNumber}...`)
     })
 })
